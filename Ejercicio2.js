025585a96a0bb3ed2cdff409161efbdfba3e8d5f
ManuelMaqueda98 <99404567+ManuelMaqueda98@users.noreply.github.com>
2022-12-08 13:40:46 +0100

    Update Ejercicio2.js

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { Button, FlatList, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function ListadoScreen() {
+function ListadoScreen({navigation}) {
     const DATA =[
         {id:'1',Nombre:'Antonio',Apellidos:'Diaz Peña',Edad:'24'},
         {id:'2',Nombre:'Adri',Apellidos:'Aparcero', Edad:'26'},
@@ -12,6 +14,8 @@ function ListadoScreen() {
     const renderitem =({item})=>(
         <View>
             <Text>{item.title}</Text>
+
+            <Button title={item.Nombre} onPress={() => navigation.navigate('DetalleUsuario',{screen : 'DetalleUsuario', params :{item}})} />
         </View>
     )
   return (
@@ -22,30 +26,46 @@ function ListadoScreen() {
             keyExtractor={item => item.Nombre}
             />
         </View>
+
   );
 }
 
 function InfoScreen() {
-  return (
-   
-  );
+  return(
+    <View><Text style={{textAlign: 'center', justifyContent: 'center' }}>Esta App te permite conocer 
+                en mas profundidad a las personas</Text></View>
+  )
 }
 
-function DetalleScreen(){
+function DetalleScreen({item}){
     return(
       
-    );
+      <View>
+        <Text>{item.Nombre}</Text>
+      </View>      
+    )
+}
+
+function UsuarioStackScreen(){
+
+  return(
+    <UsuarioStack.Navigator>
+      <UsuarioStack.Screen name="Listado de Usuarios" component={ListadoScreen} />
+      <UsuarioStack.Screen name="DetalleUsuario" component={DetalleScreen} />
+    </UsuarioStack.Navigator>
+  )
 }
 
 const Tab = createBottomTabNavigator();
-
+const UsuarioStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Listado" component={ListadoScreen} />
-        <Tab.Screen name="Info" component={InfoScreen} />
+      <Tab.Navigator screenOptions={{ }}>
+        <Tab.Screen name="Listado de Usuarios" component={ListadoScreen} />
+        <Tab.Screen name="Informacion" component={InfoScreen} />
+        
       </Tab.Navigator>
     </NavigationContainer>
   );
